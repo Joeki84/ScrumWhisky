76e873e2e1804be92012606a0e4f5482bfc7aa5f
Beerfleet <devel.janvb@gmail.com>
2015-01-14 18:33:22 +0100

    Removed Test route files from remote repo

--- config/routes/Test/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use scrum\ScotchLodge\Controllers\FormController;
-
-$app->map('/form', function() use ($em, $app) {
-  $contr = new FormController($em, $app);
-  $contr->form();
-})->via('GET', 'POST')->name('test_form');
-
