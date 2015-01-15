0d5928f209d1b570a58143846bda7e8789efb9fc
Beerfleet <devel.janvb@gmail.com>
2015-01-15 12:22:38 +0100

    removed Test route includes

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
 
 use Doctrine\Common\ClassLoader;
-use scrum\ScotchLodge\Service\Output;
 
-require_once 'bootstrap.php';
+include 'bootstrap.php';
 
 $classloader = new ClassLoader("scrum", "src");
 $classloader->register();
 
-$output = new Output();
-
 try {
-  /* routes */
-  include 'config/routes/Test/test.php';
-  include 'config/routes/Test/countries.php';
-  include 'config/routes/Test/form.php';
+  /* routes */  
   include 'config/routes/register.php';
   include 'config/routes/profile.php';
   include 'config/routes/main.php';
