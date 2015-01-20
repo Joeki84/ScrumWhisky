becf617c0c286745b7e679c746d4caad0033709a
Beerfleet <devel.janvb@gmail.com>
2015-01-20 14:57:21 +0100

    further work on mapping, finished Event mapping

--- src/scrum/ScotchLodge/Entities/Event.php
+++ src/scrum/ScotchLodge/Entities/Event.php
@@ -3,6 +3,7 @@
 namespace scrum\ScotchLodge\Entities;
 
 use scrum\ScotchLodge\Entities\Postcode;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Event entity
@@ -11,50 +12,81 @@ use scrum\ScotchLodge\Entities\Postcode;
  */
 class Event {
 
-    private $id;
-    private $title;
-    /* @var $postcode Postcode */
-    private $postcode;
-    private $address;
-    private $event_date;
-    
-    function getId(){
-        return $this->id;
-    }
-    
-    function getTitle(){
-        return $this->title;
-    }
-    
-    function getPostcode(){
-        return $this->postcode;
-    }
-    
-    function getAddress(){
-        return $this->address;
-    }
-    
-    function getEventDate(){
-        return $this->event_date;
-    }
-    
-    function setId($id){
-        $this->id = $id;
-    }
-    
-    function setTitle($title){
-        $this->title = $title;
-    }
-    
-    function setPostcode(Postcode $postcode){
-        $this->postcode = $postcode;
-    }
-    
-    function setAddress($address){
-        $this->address = $address;
-    }
-    
-    function setEventDate($event_date){
-        $this->event_date = $event_date;
-    }
+  private $id;
+  private $title;
+  /* @var $postcode Postcode */
+  private $postcode;
+  private $address;
+  private $event_date;
+  private $event_stop;
+  private $whiskys;
+
+  function __construct() {
+    $this->whiskys = new ArrayCollection();
+  }
+
+  function getId() {
+    return $this->id;
+  }
+
+  function getTitle() {
+    return $this->title;
+  }
+
+  function getPostcode() {
+    return $this->postcode;
+  }
+
+  function getAddress() {
+    return $this->address;
+  }
+
+  function getEventDate() {
+    return $this->event_date;
+  }
+
+  function setId($id) {
+    $this->id = $id;
+  }
+
+  function setTitle($title) {
+    $this->title = $title;
+  }
+
+  function setPostcode(Postcode $postcode) {
+    $this->postcode = $postcode;
+  }
+
+  function setAddress($address) {
+    $this->address = $address;
+  }
+
+  function setEventDate($event_date) {
+    $this->event_date = $event_date;
+  }
+
+  function getEvent_date() {
+    return $this->event_date;
+  }
+
+  function getWhiskys() {
+    return $this->whiskys;
+  }
+
+  function setEvent_date($event_date) {
+    $this->event_date = $event_date;
+  }
+
+  function setWhiskys($whiskys) {
+    $this->whiskys = $whiskys;
+  }
+
+  function getEvent_stop() {
+    return $this->event_stop;
+  }
+
+  function setEvent_stop($event_stop) {
+    $this->event_stop = $event_stop;
+  }
+
 }
