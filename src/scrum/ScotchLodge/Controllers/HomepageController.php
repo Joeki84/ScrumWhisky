80815885a29167a77543792bd11ea2e2aebc1c68
Beerfleet <devel.janvb@gmail.com>
2015-01-22 12:30:47 +0100

    DB fix

--- src/scrum/ScotchLodge/Controllers/HomepageController.php
+++ src/scrum/ScotchLodge/Controllers/HomepageController.php
@@ -28,7 +28,11 @@ class HomepageController extends Controller {
     
     $comments = $commentSrvc->retrieveComments(3);
     
-    $this->getApp()->render('homepage.html.twig', array('globals' => $globals, 'members' => $members, 'events' => $events, 'comments' => $comments));
+    $this->getApp()->render('homepage.html.twig', 
+        array('globals' => $globals, 
+          'members' => $members, 
+          'events' => $events, 
+          'comments' => $comments));
   }
   
   public function simplifydRoutes($routes) {
