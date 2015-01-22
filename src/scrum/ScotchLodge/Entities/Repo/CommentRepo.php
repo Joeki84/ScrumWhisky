aa866ba9b6b125cb2c4e5407f8b467b7394d738c
Beerfleet <devel.janvb@gmail.com>
2015-01-22 09:50:02 +0100

    Adding comments

---
+++ src/scrum/ScotchLodge/Entities/Repo/CommentRepo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace scrum\ScotchLodge\Entities\Repo;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * CountryRepo repository
+ *
+ * @author jan van biervliet
+ */
+class CommentRepo extends EntityRepository {
+
+  public function findLatestComments() {
+    
+  }
+
+}
