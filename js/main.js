e26b4f35032a1ae3f6fa6b3f231b4324720b4705
oliviermasudi <olivier.masudi@gmail.com>
2015-01-28 11:35:33 +0100

    jquery voor category toevoegen

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function() {
   console.log('roekoe');
 
-
+       var strtxt;         
+ 
         $('.like-btn').click(function(){
             $.ajax({
                 type:"GET",
@@ -12,6 +13,22 @@ $(function() {
                 }
             });
         });
+        
+        
+      
+            $('.editcat').click(function(){
+                  
+             strtxt='<form><input class="datum" type="text" name="category" id="category" value="';
+             strtxt+=$(this .catname).text();
+             strtxt+='"/><input class="datum" type="hidden" name="categoryid" id="terugdatum" /><input type="submit" value="Save" id="save" /><input type="submit" value="Cancel" id="cancel" /></form>';
+             $(this).parent().html(strtxt);
+             console.log($(this).parent().text());
+ 
+ 
+        });
+        
+        
+        
 
 
 
