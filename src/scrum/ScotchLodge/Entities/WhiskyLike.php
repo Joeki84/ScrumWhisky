91ab546a921b9d4eae0e9d714161cea1c4e4ccf3
Beerfleet <devel.janvb@gmail.com>
2015-01-29 14:09:54 +0100

    Orm + entity extra field whisky like state

--- src/scrum/ScotchLodge/Entities/WhiskyLike.php
+++ src/scrum/ScotchLodge/Entities/WhiskyLike.php
@@ -9,9 +9,18 @@ namespace scrum\ScotchLodge\Entities;
  */
 class WhiskyLike {
 
+  const LIKED = 1;
+  const DISLIKED = -1;
+  const CLEAR = 0;
+  
   private $id;
   private $whisky;
   private $user;
+  private $state;     
+
+  function __construct() {
+    $this->state = 0;
+  }
 
   function getId() {
     return $this->id;
@@ -35,6 +44,26 @@ class WhiskyLike {
 
   function setUser($user) {
     $this->user = $user;
+  }
+
+  function getState() {
+    return $this->state;
+  }
+
+  function setState($state) {
+    $this->state = $state;
+  }
+  
+  function setLike() {
+    $this->state = 1;
+  }
+  
+  function setDislike() {
+    $this->state = -1;
+  }
+  
+  function setClearState() {
+    $this->state = 0;
   }
 
 }
