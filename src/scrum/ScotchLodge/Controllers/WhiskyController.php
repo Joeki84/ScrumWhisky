6b79bba5b65d995b1384c924adfde5ad43570c85
Beerfleet <devel.janvb@gmail.com>
2015-01-30 10:39:51 +0100

    merge

--- src/scrum/ScotchLodge/Controllers/WhiskyController.php
+++ src/scrum/ScotchLodge/Controllers/WhiskyController.php
@@ -14,30 +14,62 @@ use scrum\ScotchLodge\Service\Country\CountryService;
 use scrum\ScotchLodge\Service\Category\CategoryService;
 use scrum\ScotchLodge\Entities\Whisky;
 use scrum\ScotchLodge\Entities\User;
+use scrum\ScotchLodge\Service\WhiskyLike\WhiskyLikeService;
 
 /**
  * WhiskyController controller
  * 
  * @author joeri broos 
  */
-class WhiskyController extends Controller{
-    
-    /*@var $whiskysrv WhiskyService */
-    private $whiskysrv;
-    private $em;
-    private $app;
+class WhiskyController extends Controller {
+  /* @var $whiskysrv WhiskyService */
 
-    public function __construct($em, $app) {
-        parent::__construct($em, $app);
-        $this->whiskysrv = new WhiskyService($em, $app);
-        $this->em = $em;
-        $this->app = $app;
-    }
-    
-    /**
-     * Render the page to add a new whisky.
-     */
-    public function addWhisky(){
+  private $whiskysrv;
+  private $em;
+  private $app;
+
+  public function __construct($em, $app) {
+    parent::__construct($em, $app);
+    $this->whiskysrv = new WhiskyService($em, $app);
+    $this->em = $em;
+    $this->app = $app;
+  }
+
+  /**
+   * Render the page to add a new whisky.
+   */
+  public function addWhisky() {
+    $regsrv = new RegionService($this->em, $this->app);
+    $regions = $regsrv->getRegions();
+    $distsrv = new DistilleryService($this->em, $this->app);
+    $distillerys = $distsrv->getDistillerys();
+    $barsrv = new BarrelService($this->em, $this->app);
+    $barrels = $barsrv->getBarrels();
+    $blendsrv = new BlendService($this->em, $this->app);
+    $blends = $blendsrv->getBlends();
+    $countrysrv = new CountryService($this->em, $this->app);
+    $countries = $countrysrv->getCountries();
+    $categorysrv = new CategoryService($this->em, $this->app);
+    $categories = $categorysrv->getCategories();
+
+    $globals = $this->getGlobals();
+    $this->getApp()->render('Whisky/new_whisky.html.twig', array('globals' => $globals, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries, 'categories' => $categories));
+  }
+
+  /**
+   * Render the page to insert a whisky.
+   */
+  public function insertWhisky() {
+    try {
+      $user = $this->getUser();
+      $whisky = $this->whiskysrv->addWhisky($user);
+      if ($whisky) {
+        $app = $this->app;
+        $app->flash('info', 'Successful added whisky ' . $whisky->getName() . ' .');
+        $app->redirect($app->urlFor('main_page'));
+      }
+      else {
+        $errors = $this->whiskysrv->getErrors();
         $regsrv = new RegionService($this->em, $this->app);
         $regions = $regsrv->getRegions();
         $distsrv = new DistilleryService($this->em, $this->app);
@@ -48,51 +80,50 @@ class WhiskyController extends Controller{
         $blends = $blendsrv->getBlends();
         $countrysrv = new CountryService($this->em, $this->app);
         $countries = $countrysrv->getCountries();
+        $globals = $this->getGlobals();
         $categorysrv = new CategoryService($this->em, $this->app);
         $categories = $categorysrv->getCategories();
-        
-        $globals = $this->getGlobals();
-        $this->getApp()->render('Whisky/new_whisky.html.twig', array('globals' => $globals, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries, 'categories' => $categories));
+        $this->getApp()->render('Whisky/new_whisky.html.twig', array('globals' => $globals, 'errors' => $errors, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries, 'categories' => $categories));
+      }
+    } catch (Exception $ex) {
+      $this->getApp()->render('probleem.twig.html');
     }
+  }
 
-    /**
-     * Render the page to insert a whisky.
-     */
-    public function insertWhisky(){
-        try{
-            $user = $this->getUser();
-            $whisky = $this->whiskysrv->addWhisky($user);
-            if($whisky){
-                $app = $this->app;
-                $app->flash('info','Successful added whisky ' . $whisky->getName() .' .');
-                $app->redirect($app->urlFor('main_page'));
-            }else{
-                $errors = $this->whiskysrv->getErrors();
-                $regsrv = new RegionService($this->em, $this->app);
-                $regions = $regsrv->getRegions();
-                $distsrv = new DistilleryService($this->em, $this->app);
-                $distillerys = $distsrv->getDistillerys();
-                $barsrv = new BarrelService($this->em, $this->app);
-                $barrels = $barsrv->getBarrels();
-                $blendsrv = new BlendService($this->em, $this->app);
-                $blends = $blendsrv->getBlends();
-                $countrysrv = new CountryService($this->em, $this->app);
-                $countries = $countrysrv->getCountries();                
-                $globals = $this->getGlobals();
-                $categorysrv = new CategoryService($this->em, $this->app);
-                $categories = $categorysrv->getCategories();        
-                $this->getApp()->render('Whisky/new_whisky.html.twig', array('globals' => $globals, 'errors' => $errors, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries, 'categories' => $categories));
-            }
-        } catch (Exception $ex) {
-            $this->getApp()->render('probleem.twig.html');
-        }
-    }
-    
-    /**
-     * Render the page to add a new whisky.
-     */
-    public function editWhisky($id){
-        $whisky = $this->whiskysrv->retrieveWhiskyById($id);        
+  /**
+   * Render the page to add a new whisky.
+   */
+  public function editWhisky($id) {
+    $whisky = $this->whiskysrv->retrieveWhiskyById($id);
+    $regsrv = new RegionService($this->em, $this->app);
+    $regions = $regsrv->getRegions();
+    $distsrv = new DistilleryService($this->em, $this->app);
+    $distillerys = $distsrv->getDistillerys();
+    $barsrv = new BarrelService($this->em, $this->app);
+    $barrels = $barsrv->getBarrels();
+    $blendsrv = new BlendService($this->em, $this->app);
+    $blends = $blendsrv->getBlends();
+    $countrysrv = new CountryService($this->em, $this->app);
+    $countries = $countrysrv->getCountries();
+    $globals = $this->getGlobals();
+    $this->getApp()->render('Whisky/edit_whisky.html.twig', array('globals' => $globals, 'whisky' => $whisky, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries));
+  }
+
+  /**
+   * Render the page to insert a whisky.
+   * @param int $id 
+   */
+  public function updateWhisky($id) {
+    try {
+      $whisky_old = $this->whiskysrv->retrieveWhiskyById($id);
+      $whisky = $this->whiskysrv->updateWhisky($whisky_old);
+      if ($whisky) {
+        $app = $this->app;
+        $app->flash('info', 'Successful update whisky ' . $whisky->getName() . ' .');
+        $app->redirect($app->urlFor('main_page'));
+      }
+      else {
+        $errors = $this->whiskysrv->getErrors();
         $regsrv = new RegionService($this->em, $this->app);
         $regions = $regsrv->getRegions();
         $distsrv = new DistilleryService($this->em, $this->app);
@@ -102,107 +133,79 @@ class WhiskyController extends Controller{
         $blendsrv = new BlendService($this->em, $this->app);
         $blends = $blendsrv->getBlends();
         $countrysrv = new CountryService($this->em, $this->app);
-        $countries = $countrysrv->getCountries();        
+        $countries = $countrysrv->getCountries();
         $globals = $this->getGlobals();
-        $this->getApp()->render('Whisky/edit_whisky.html.twig', array('globals' => $globals, 'whisky' => $whisky, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries));
+        $this->getApp()->render('Whisky/edit_whisky.html.twig', array('globals' => $globals, 'errors' => $errors, 'whisky' => $whisky_old, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries));
+      }
+    } catch (Exception $ex) {
+      $this->getApp()->render('probleem.twig.html');
     }
-    
-    /**
-     * Render the page to insert a whisky.
-     * @param int $id 
-     */
-    public function updateWhisky($id){
-        try{
-            $whisky_old = $this->whiskysrv->retrieveWhiskyById($id);
-            $whisky = $this->whiskysrv->updateWhisky($whisky_old);
-            if($whisky){
-                $app = $this->app;
-                $app->flash('info','Successful update whisky ' . $whisky->getName() .' .');
-                $app->redirect($app->urlFor('main_page'));
-            }else{
-                $errors = $this->whiskysrv->getErrors();
-                $regsrv = new RegionService($this->em, $this->app);
-                $regions = $regsrv->getRegions();
-                $distsrv = new DistilleryService($this->em, $this->app);
-                $distillerys = $distsrv->getDistillerys();
-                $barsrv = new BarrelService($this->em, $this->app);
-                $barrels = $barsrv->getBarrels();
-                $blendsrv = new BlendService($this->em, $this->app);
-                $blends = $blendsrv->getBlends();
-                $countrysrv = new CountryService($this->em, $this->app);
-                $countries = $countrysrv->getCountries();                
-                $globals = $this->getGlobals();
-                $this->getApp()->render('Whisky/edit_whisky.html.twig', array('globals' => $globals, 'errors' => $errors, 'whisky' => $whisky_old, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'blends' => $blends, 'countries' => $countries));
-            }
-        } catch (Exception $ex) {
-            $this->getApp()->render('probleem.twig.html');
-        }
-    }
-    
-    
-    /* Olivier */
-    
-    public function advanced_search_whisky(){
-        $regsrv = new RegionService($this->em, $this->app);
-        $regions = $regsrv->getRegions();
-        
-        $distsrv = new DistilleryService($this->em, $this->app);
-        $distillerys = $distsrv->getDistillerys();
-        
-        $bottlers = $distsrv->getDistillerys();
-        $barsrv = new BarrelService($this->em, $this->app);       
-        $barrels = $barsrv->getBarrels();
-        
-        $blendsrv = new BlendService($this->em, $this->app);
-        $blends = $blendsrv->getBlends();
-        
-        $globals = $this->getGlobals();
-        $this->getApp()->render('Whisky/advanced_search.html.twig', array('globals' => $globals,  'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'bottlers' => $bottlers, 'blends' => $blends));
-    }
-    
-    public function advanced_search_whisky_result(){
+  }
 
-        $globals = $this->getGlobals();        
-        $whisky=$this->whiskysrv->advanced_search_whisky_result();
-        $this->getApp()->render('Whisky/advanced_search_result.html.twig', array('globals' => $globals,  'whiskys'  => $whisky));
-    }
-    
-    public function show_whisky_by_id($id) {
-         try{
+  /* Olivier */
 
-        $globals = $this->getGlobals();  
-        $whisky = $this->whiskysrv->retrieveWhiskyById($id);
-        $whisky = $this->whiskysrv->ViewWhisky($whisky);
-        if($whisky){
-              $this->getApp()->render('Whisky/show_whisky_by_id.html.twig', array('globals' => $globals,  'whisky' => $whisky));
-          }
-          else{
-                $app = $this->getApp();
-                $app->flash('error', 'The requested whisky does not exist');
-                $app->redirectTo('error_404');
-            }
-        } catch (Exception $ex) {
-            $this->getApp()->render('probleem.twig.html');
-        }
-          
-    }
-    
-    /**** Olivier */
-    
-    public function ajax_addComment($whisky_id) {
-      /* @var  $app Slim */
-      $app = $this->app;
-      if ($app->request()->isAjax()) {
-        $message = $app->request->post('text');
-        
-        if (trim($message) != '') {
-          $srv = $this->whiskysrv;
-          $srv->addWhiskyComment($whisky_id, $this->getUser(), $message);
-        }
-        
-      } else {
+  public function advanced_search_whisky() {
+    $regsrv = new RegionService($this->em, $this->app);
+    $regions = $regsrv->getRegions();
+
+    $distsrv = new DistilleryService($this->em, $this->app);
+    $distillerys = $distsrv->getDistillerys();
+
+    $bottlers = $distsrv->getDistillerys();
+    $barsrv = new BarrelService($this->em, $this->app);
+    $barrels = $barsrv->getBarrels();
+
+    $blendsrv = new BlendService($this->em, $this->app);
+    $blends = $blendsrv->getBlends();
+
+    $globals = $this->getGlobals();
+    $this->getApp()->render('Whisky/advanced_search.html.twig', array('globals' => $globals, 'regions' => $regions, 'distillerys' => $distillerys, 'barrels' => $barrels, 'bottlers' => $bottlers, 'blends' => $blends));
+  }
+
+  public function advanced_search_whisky_result() {
+
+    $globals = $this->getGlobals();
+    $whisky = $this->whiskysrv->advanced_search_whisky_result($this->em, $this->app);
+    $whiskylikesrv = new WhiskyLikeService($this->em, $this->app);
+    $whiskylike = $whiskylikesrv->isalreadyLikeMulti($globals["user"]->getId());
+    $this->getApp()->render('Whisky/advanced_search_result.html.twig', array('globals' => $globals, 'whiskys' => $whisky, 'whiskylike' => $whiskylike));
+  }
+
+  public function show_whisky_by_id($id) {
+    try {
+
+      $globals = $this->getGlobals();
+      $whisky = $this->whiskysrv->retrieveWhiskyById($id);
+      $whisky = $this->whiskysrv->ViewWhisky($whisky);
+      if ($whisky) {
+        $this->getApp()->render('Whisky/show_whisky_by_id.html.twig', array('globals' => $globals, 'whisky' => $whisky));
+      }
+      else {
+        $app = $this->getApp();
+        $app->flash('error', 'The requested whisky does not exist');
         $app->redirectTo('error_404');
       }
+    } catch (Exception $ex) {
+      $this->getApp()->render('probleem.twig.html');
     }
-    
+  }
+
+  /*   * ** Olivier */
+
+  public function ajax_addComment($whisky_id) {
+    /* @var  $app Slim */
+    $app = $this->app;
+    if ($app->request()->isAjax()) {
+      $message = $app->request->post('text');
+
+      if (trim($message) != '') {
+        $srv = $this->whiskysrv;
+        $srv->addWhiskyComment($whisky_id, $this->getUser(), $message);
+      }
+    }
+    else {
+      $app->redirectTo('error_404');
+    }
+  }
+
 }
--- src/scrum/ScotchLodge/Service/WhiskyLike/WhiskyLikeService.php
+++ src/scrum/ScotchLodge/Service/WhiskyLike/WhiskyLikeService.php
@@ -8,69 +8,81 @@ use scrum\ScotchLodge\Entities\WhiskyLike;
 use \scrum\ScotchLodge\Service\Profile\ProfileService;
 use \scrum\ScotchLodge\Service\Whisky\WhiskyService;
 
-
-
 /**
  * WhyskiLikeService
  *
  * @author olivier masudi
  */
 class WhiskyLikeService {
-    
-    private $em;
-    private $app;
 
-    public function __construct($em, $app) {
-      $this->em = $em;
-      $this->app = $app;
+  private $em;
+  private $app;
+
+  public function __construct($em, $app) {
+    $this->em = $em;
+    $this->app = $app;
+  }
+
+  public function addlike() {
+
+    $whiskyid = $this->app->request->post('whiskyid');
+    $likeid = $this->app->request->post('likeid');
+    $userid = $this->app->request->post('userid');
+    $status = $this->app->request->post('status');
+    $usersrv = new ProfileService($this->em, $this->app);
+    $user = $usersrv->searchUserById($userid);
+    $whiskysrv = new WhiskyService($this->em, $this->app);
+    $whisky = $whiskysrv->retrieveWhiskyById($whiskyid);
+    $WhiskyLike = $this->isalreadyLike($whiskyid, $userid);
+    $c = $WhiskyLike;
+    if ($WhiskyLike) {
+//$WhiskyLike->setUser($user);
+//$WhiskyLike->setWhisky($whisky);
+      $WhiskyLike->setState($status);
     }
-
-    
-public function addlike(){
-    
-   $whiskyid=$this->app->request->post('whiskyid');
-   $likeid=$this->app->request->post('likeid');
-   $userid=$this->app->request->post('userid');
-   $status=$this->app->request->post('status');
-   
-   $usersrv = new ProfileService($this->em, $this->app);
-   $user = $usersrv->searchUserById($userid);
-
-   $whiskysrv = new WhiskyService($this->em, $this->app);
-   $whisky = $whiskysrv->retrieveWhiskyById($whiskyid);
-   
-   
-   $WhiskyLike = $this->em->find('scrum\ScotchLodge\Entities\WhiskyLike',$likeid );
-   if($WhiskyLike != NULL)
-   {
-         $WhiskyLike->setUser($user);
-         $WhiskyLike->setWhisky($whisky);
-         $WhiskyLike->setState($status);
-         //$this->em->merge($WhiskyLike);
-   }
-   else
-   {
-       $WhiskyLike= new WhiskyLike();
-       $WhiskyLike->setUser($user);
-       $WhiskyLike->setWhisky($whisky);
-       $WhiskyLike->setState($status);
-       
-   }    
-
-      $this->em->persist($WhiskyLike);
-      $this->em->flush();    
-
-   return $WhiskyLike;  
-   
-}    
-    public function isalreadyLike($id_comment,$id_user){                      
-       $like = $this->em->getRepository('scrum\ScotchLodge\Entities\WhiskyLike')->findBy(array('user_id'=> $id_user  ,'Whisky_id' => $id_comment));
-       if(count($like)> 0){
-            return true;
-        }else{
-            return null;
-        }
-        
+    else {
+      $WhiskyLike = new WhiskyLike();
+      $WhiskyLike->setUser($user);
+      $WhiskyLike->setWhisky($whisky);
+      $WhiskyLike->setState($status);
     }
-    
+    $this->em->persist($WhiskyLike);
+    $this->em->flush();
+    return $WhiskyLike;
+  }
+
+  public function isalreadyLike($id_comment, $id_user) {
+    $like = $this->em->getRepository('scrum\ScotchLodge\Entities\WhiskyLike')->findBy(array('user' => $id_user, 'whisky' => $id_comment));
+    if (count($like) > 0) {
+      return $like[0];
+    }
+    else {
+      return null;
+    }
+  }
+
+  public function isalreadyLikeMulti($id_user) {
+    $like = $this->em->getRepository('scrum\ScotchLodge\Entities\WhiskyLike')->findBy(array('user' => $id_user));
+    if (count($like) > 0) {
+      foreach ($like as $value) {
+        $index = "like" . $value->getWhisky()->getId();
+        $tab[$index] = $value->getState();
+      }
+      return $tab;
+    }
+    else {
+      return null;
+    }
+  }
+
+  public function retrieveWhiskyLikeById($id) {
+    $whiskyLike = $this->em->getRepository('scrum\ScotchLodge\Entities\WhiskyLike')->find($id);
+    if (count($whiskyLike) > 0) {
+      return $whiskyLike;
+    }
+    else {
+      return null;
+    }
+  }
+
 }
